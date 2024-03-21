a3f4eccf79e21eccb3c31d19af89fca78f65b488
João Athayde <joaovictoragrilo@gmail.com>
2024-03-21 19:58:58 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: '@react-native',
+  requireConfigFile: false,
+  babelOptions: {
+  presets: ["@babel/preset-react"]}
+}
+
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Text } from "react-native"
+import PrimeiroComponente from "./components/PrimeiroComponente";
+
+export default () => (
+    <PrimeiroComponente/>
+)
